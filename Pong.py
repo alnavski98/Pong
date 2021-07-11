1cff58d516b75cadbcb374095e828dd0947bbc41
alnavski98 <alnavski98@gmail.com>
2021-07-11 23:11:29 +0200

    Changed order of the main_menu and main function

--- Pong.py
+++ Pong.py
@@ -3,16 +3,19 @@ import time
 import random
 from Vertical_bar import Vertical_bar
 from Ball import Ball
+import sys
 from pygame.event import get
+
+# Fix the bug with the ball going inside the vertical bars
 
 # Initialize pygame font
 pygame.font.init()
 
 # Screen settings
-width, height = 1500, 750
-bg = 0, 0, 0
-text_color = 255, 255, 255
-win = pygame.display.set_mode((width, height))
+width, height = 800, 600
+background = (0, 0, 0)
+text_color = (255, 255, 255)
+window = pygame.display.set_mode((width, height))
 
 # Bar dimensions
 bar_width = 25
@@ -27,6 +30,31 @@ vertical_direction = ["Up" , "Down"]
 
 pygame.display.set_caption("Pong game")
 
+def main_menu():
+    main_title_font1 = pygame.font.SysFont("comicsans", 100)
+    subtitle_font2 = pygame.font.SysFont("comicsans", 70)
+    subsubtitle_font3 = pygame.font.SysFont("comicsans", 40)
+    run = True
+    while run:
+        window.fill(background)
+        main_title_label = main_title_font1.render("PONG!", 1, text_color)
+        subtitle_label = subtitle_font2.render("Press space bar to begin", 1, text_color)
+        subsubtitle_label = subsubtitle_font3.render("W and S for left bar, and up and down arrow for right", 1, text_color)
+        window.blit(main_title_label, (width / 2 - main_title_label.get_width() / 2, 200))
+        window.blit(subtitle_label, (width / 2 - subtitle_label.get_width() / 2, 450))
+        window.blit(subsubtitle_label, (width / 2 - subsubtitle_label.get_width() / 2, 550))
+        pygame.display.update()
+        keys = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if keys[pygame.K_SPACE]:
+                main()
+            # Add the possibility for adjusting the resolution based off of the users preference
+            if keys[pygame.K_TAB]:
+                pass
+    pygame.quit()
+
 def main():
     run = True
     FPS = 60
@@ -40,8 +68,8 @@ def main():
     ball_vel_x = 7
     ball_vel_y = 7
 
-    p1 = Vertical_bar(50, height / 2 - 100) # Left bar
-    p2 = Vertical_bar(1425, height / 2 - 100) # Right bar
+    p1 = Vertical_bar(20, height / 2 - 100) # Left bar
+    p2 = Vertical_bar(width - 25 - 20, height / 2 - 100) # Right bar
 
     ball = Ball(width / 2 - ball_width, height / 2 - ball_height)
 
@@ -58,17 +86,17 @@ def main():
     clock = pygame.time.Clock()
 
     def redraw_window():
-        win.fill(bg)
+        window.fill(background)
         score_font = pygame.font.SysFont("comicsans", 50)
         p1_score_label = score_font.render(f"P1 points: {p1_points}", 1, text_color)
         p2_score_label = score_font.render(f"P2 points: {p2_points}", 1, text_color)
-        win.blit(p1_score_label, (0.2 * width, 10))
-        win.blit(p2_score_label, (0.65 * width, 10))
-        p1.draw(win)
-        p2.draw(win)
+        window.blit(p1_score_label, (0.2 * width, 10))
+        window.blit(p2_score_label, (0.65 * width, 10))
+        p1.draw(window)
+        p2.draw(window)
         # pygame.display.flip()
 
-        ball.draw(win)
+        ball.draw(window)
         pygame.display.update()
 
         ball_collide = False
@@ -98,11 +126,11 @@ def main():
         # Update ball's position
         ball.move(ball_vel_x, ball_vel_y)
 
-        # Bounce of top or bottom of the window
+        # Bounce of top or bottom of the windowdow
         if ball.y <= 0 or ball.y + ball_height >= height:
             ball_vel_y *= -1
 
-        # Bounce of left or right side of the window
+        # Bounce of left or right side of the windowdow
         if ball.x <= 0 or ball.x + ball_width >= width:
             ball_vel_x *= -1
             if ball.x <= 0:
@@ -118,25 +146,6 @@ def main():
         if ball_collide_left or ball_collide_right:
             ball_vel_x *= -1
 
-    pygame.quit()
-
-def main_menu():
-    main_title_font1 = pygame.font.SysFont("comicsans", 100)
-    subtitle_font2 = pygame.font.SysFont("comicsans", 70)
-    run = True
-    while run:
-        win.fill(bg)
-        main_title_label = main_title_font1.render("PONG!", 1, text_color)
-        subtitle_label = subtitle_font2.render("Press space bar to begin", 1, text_color)
-        win.blit(main_title_label, (width / 2 - main_title_label.get_width() / 2, 200))
-        win.blit(subtitle_label, (width / 2 - subtitle_label.get_width() / 2, 450))
-        pygame.display.update()
-        keys = pygame.key.get_pressed()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            if keys[pygame.K_SPACE]:
-                main()
     pygame.quit()
 
 main_menu()
